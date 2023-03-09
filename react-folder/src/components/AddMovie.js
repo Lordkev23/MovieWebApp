f2fff7a4747c59bc012826bd4a08cbdbd46140aa
Kevin <kevdaviman.23@gmail.com>
2023-03-09 11:08:58 -0500

    add form to create movies

--- react-folder/src/components/AddMovie.js
+++ react-folder/src/components/AddMovie.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddMovie = () =>{
+
+    const title = "Add Movie";
+    const [movieState, setMovieState] = useState({
+        title: '',
+        description: ''
+    });
+
+    const getFormData = (event)=>{
+        event.preventDefault();
+
+        //get form data
+        let target = event.target;
+        let title = target.title.value;
+        let description = target.description.value;
+
+        //create object about save movie
+        let movie = {
+            id: new Date().getTime(),
+            title: title,
+            description: description
+        }
+        
+        setMovieState(movie);
+    }
+
     return(
     <>
         <div className="add">
-                <h3 className="title">Add Movie</h3>
-                <form>
-                    <input type="text" id="title" placeholder="Title"/>
-                    <textarea id="description" placeholder="Description"></textarea>
-                    <input type="submit" id="save" value="Save"/>
+                <h3 className="title">{title}</h3>
+
+                <form onSubmit={getFormData}>
+                    <input type="text"
+                            id="title"
+                            name="title"
+                            placeholder="Title"/>
+
+                    <textarea id="description"
+                                name="description"
+                               placeholder="Description"></textarea>
+
+                    <input type="submit"
+                            id="save"
+                            value="Save"/>
                 </form>
             </div>
     </>
